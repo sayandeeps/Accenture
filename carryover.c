7d439fcb7f2aa18bb2365cb65c3dcd8e97424177
Sayandeep Sharma <89361557+sayandeeps@users.noreply.github.com>
2024-07-31 04:31:35 +0530

    Update carryover.c

--- carryover.c
+++ carryover.c
@@ -1,38 +1,50 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 
-int length(int n) {
-    int len = 0;
-    while (n!=0) {
-        len++;
-        n/=10;
-    }
-    return len;
-}
-
-int NumberOfCarries(int num1 , int num2){
-    int carry = 0;  
-    int count = 0;  
-
-    while (num1 != 0 || num2 != 0 || carry != 0) {
-        int digit1 = num1 % 10;  
-        int digit2 = num2 % 10;  
-        int sum = digit1 + digit2 + carry;
-        if (sum >= 9) {
-            carry = 1;  
-            count++;    
-        } else {
-            carry = 0;  
+int NumberOfCarries(int num1 , int num2) {
+    int carry =0,count=0;
+    while(num1!=0 && num2!=0){
+        int a1 = num1%10;
+        int a2 = num2%10;
+        if(a1+a2 +carry >9){
+            carry =1;
+            count++;
+        }else{
+            carry =0 ;
         }
-
-        num1 /= 10;
-        num2 /= 10;
+        num1/=10;
+        num2/=10;
     }
-    return count;  
+    while(num1!=0){
+        int a1 = num1%10;
+      
+        if(a1 +carry >9){
+            carry =1;
+            count++;
+        }else{
+            carry =0 ;
+        }
+        num1/=10;
+    }
+    while(num2!=0){
+        int a1 = num2%10;
+      
+        if(a1 +carry >9){
+            carry =1;
+            count++;
+        }else{
+            carry =0 ;
+        }
+        num2/=10;
+    }
+    return count;
 }
 
 int main() {
-    int n1,n2;
-    scanf("%d%d",&n1,&n2);
-    printf("%d",NumberOfCarries(n1,n2));
+    int num1=23;
+    int num2=563;
+  
+    int result = NumberOfCarries(num1, num2);
+    printf("%d\n", result);
+
+    return 0;
 }
